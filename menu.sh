97b6d8a8880dfb5656664fc54e554c32e6a7dc5c
Martin-Winfred <blakewinfred@gmail.com>
2022-06-06 23:37:40 +0800

    mearge file to one

--- menu.sh
+++ menu.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #Identify USER
 if [ $EUID -ne 0 ]; then
-    echo "This script must be run as root" 1>2
+    echo -e "This script must be run as root\nAnd does not start with sudo command" 1>&2
     exit 1
 fi
 
-wget -N --no-check-certificate https://raw.githubsercontent.com/Jerry-Zheng-bilibili/dd-swap-/main/ddswap.sh
 
 clear
 
@@ -17,27 +16,21 @@ echo "3) Enable Swap"
 
 echo "4) Unstall Swap"
 
-echo "5) Only Generate Swap"
-
 echo "0) Exit"
 read choise
-
-#createSwap
-function createSwap() (
-    clear
-    bash $(pwd)/ddswap.sh
-)
 
 #offSwap
 function offSwap() (
     clear
     swapoff /mnt/swap
+    bash menu.sh
 )
 
 #onSwap
 function onSwap() (
     clear
     swapon /mnt/swap
+    bash menu.sh
 )
 
 #uninstallSwap
@@ -45,21 +38,24 @@ function uninstallSwap() (
     clear
     swapoff /mnt/swap
     rm /mnt/swap
+    bash menu.sh
 )
 
 #generateSwap
-function generateSwap() (
+function createSwap() (
     clear
     if [ "$1" == "-D" ]; then 
         dd if=/dev/zero of=/mnt/swap bs=1M count=2048 
     else 
         echo -e "Input how many swap you want to make (MB)\n Eg.2048" 
         echo "Warning Do not use oversized space" 
+        echo "You need to manually enable it"
         read coun 
         dd if=/dev/zero of=/mnt/swap bs=1M count=$coun
     fi
 
     mkswap /mnt/swap
+    bash menu.sh
 )
 
 case $choise in
@@ -71,8 +67,6 @@ case $choise in
     onSwap;;
     4)
     uninstallSwap;;
-    5)
-    generateSwap;;
     0)
     echo "Bye"
     exit 1;;
